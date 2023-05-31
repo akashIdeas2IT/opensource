cc874620fd465bd872c4c5479e02596eb0082bfa
akashIdeas2IT <110529484+akashIdeas2IT@users.noreply.github.com>
2023-05-31 17:37:22 +0530

    Feature: GitHub Actions
          -  Coverage test
          -  Under 80%

--- src/test/java/com/skyessentials/opensource/service/impl/CalculationImplTest.java
+++ src/test/java/com/skyessentials/opensource/service/impl/CalculationImplTest.java
@@ -1,25 +1,25 @@
-package com.skyessentials.opensource.service.impl;
-
-import com.skyessentials.opensource.service.Calculation;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
-import org.springframework.beans.factory.annotation.Autowired;
-
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
-class CalculationImplTest {
-
-    @InjectMocks
-    CalculationImpl calculation;
-
-    @Test
-    void add() {
-        int sum = calculation.add(2, 3);
-        Assertions.assertEquals(5, sum);
-    }
-}
+//package com.skyessentials.opensource.service.impl;
+//
+//import com.skyessentials.opensource.service.Calculation;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.mockito.junit.jupiter.MockitoSettings;
+//import org.mockito.quality.Strictness;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//@ExtendWith(MockitoExtension.class)
+//@MockitoSettings(strictness = Strictness.LENIENT)
+//class CalculationImplTest {
+//
+//    @InjectMocks
+//    CalculationImpl calculation;
+//
+//    @Test
+//    void add() {
+//        int sum = calculation.add(2, 3);
+//        Assertions.assertEquals(5, sum);
+//    }
+//}
